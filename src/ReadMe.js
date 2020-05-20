3808a3f91ff94a08cdfde875799d5df0d7211450
Mark Miro <markmirony+github@gmail.com>
2020-05-20 14:03:40 -0700

    Update readme

--- src/ReadMe.js
+++ src/ReadMe.js
@@ -3,11 +3,10 @@ import React from "react";
 export function ReadMe() {
   return (
     <p>
-      Keeping state of a nested tree in a reducer can be unperformant. Try
-      typing into the input fields to get a sense for how laggy it feels. It's
-      easy to solve this problem by debouncing the text input, but can it be
-      done in other ways? What if some nodes depended on others? What if instead
-      of text, it was some other property?
+      This is my playground for doing performant drag/drop and complex state
+      management. Take a nested node and try to drag it. Type into the different
+      nodes. Is it performant? Is there jank? If so, what's causing it? Does the
+      jank depend on the number of nodes?
     </p>
   );
 }
